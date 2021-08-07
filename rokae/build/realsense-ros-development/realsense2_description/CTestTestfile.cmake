486076d04b8acf2a7c18decb6d0ccecac89329a9
perferom123 <perferom@163.com>
2021-08-07 12:08:03 +0800

    删除

--- rokae/build/realsense-ros-development/realsense2_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nuc/Desktop/rokae_robot/rokae/src/realsense-ros-development/realsense2_description
-# Build directory: /home/nuc/Desktop/rokae_robot/rokae/build/realsense-ros-development/realsense2_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
