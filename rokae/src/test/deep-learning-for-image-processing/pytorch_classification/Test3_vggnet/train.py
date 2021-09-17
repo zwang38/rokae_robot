67734ff5482bbb56bddaed8d30448bb919b71280
perferom123 <perferom@163.com>
2021-09-17 09:52:57 +0800

    vgg image resize edit

--- rokae/src/test/deep-learning-for-image-processing/pytorch_classification/Test3_vggnet/train.py
+++ rokae/src/test/deep-learning-for-image-processing/pytorch_classification/Test3_vggnet/train.py
@@ -15,7 +15,9 @@ def main():
     print("using {} device.".format(device))
 
     data_transform = {
-        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+        "train": transforms.Compose([
+                                     transforms.Resize((256,256)), #先调整图片大小至256x256
+	                                 transforms.RandomCrop((224,224)), #再随机裁剪到224x224
                                      transforms.RandomHorizontalFlip(),
                                      transforms.ToTensor(),
                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
